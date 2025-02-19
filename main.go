d97c1c93b5207e979155bbb1b20678dd50394bb4
Bagusbimawan <bagusbimawansembodo@gmail.com>
2025-02-19 20:43:11 +0700

    feat(main): add CORS configuration to allow all origins

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"backend-deep-learning/route"
 
 	"github.com/gofiber/fiber/v2"
-	
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Allow requests from any origin
+		AllowHeaders: "origin, content-type, accept",
+	}))
 
-	app:= fiber.New()
 	database.Database()
 	route.UserRoute(app)
 	app.Listen(":8000")
